7d2291ee9c9813e2440c2fa676bfbcb1f0d372ee
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-06 22:09:43 -0500

    Integra useLogin con validaciones y actualiza estilos

--- frontend/components/forms/loginForm/loginForm.js
+++ frontend/components/forms/loginForm/loginForm.js
@@ -1,11 +1,20 @@
-import Button from "../../ui/Button/Button";
-import Input from "../../ui/Input/Input";
-import styles from "./loginForm.module.css";
-export default function LoginForm(){
-    return(
-        <form className={styles.formLogin}>
-            <Input type="email" placeholder="Correo:" name="email" id="email" className={styles.input}/>
-            <Input type="password" placeholder="Contraseña:" name="password" id="password" className={styles.input}/>
+'use client'
+import { useLogin } from './useLogin'
+import Button from "../../ui/Button/Button"
+import Input from "../../ui/Input/Input"
+import styles from "./loginForm.module.css"
+
+export default function LoginForm() {
+    const { formData, errors, handleChange, handleSubmit } = useLogin()
+    
+    return (
+        <form className={styles.formLogin} onSubmit={handleSubmit}>
+            <Input type="email" placeholder="Correo:" name="correo" id="correo" className={styles.input} value={formData.correo} onChange={handleChange} />
+            {errors.correo && <p className={styles.error}>{errors.correo}</p>}
+
+            <Input type="password" placeholder="Contraseña:" name="contraseña" id="contraseña" className={styles.input} value={formData.contraseña} onChange={handleChange} />
+            {errors.contraseña && <p className={styles.error}>{errors.contraseña}</p>}
+
             <Button type="submit" className={styles.btn} >Entrar</Button>
         </form>
     )
