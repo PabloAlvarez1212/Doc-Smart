83bf9e45fb67377959093bfbdf803eade969921a
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-08 17:32:02 -0500

    feat: mejora UI login y agrega links de navegacion

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import styles from "./login.module.css"
 import LoginForm from "../../../../components/forms/loginForm/loginForm"
+import Link from "next/link"
 export default function login() {
     return (
         <div className={styles.mainLogin}>
@@ -12,6 +13,10 @@ export default function login() {
                 <h2>Inicia sesión:</h2>
                 <div className={styles.form}>
                     <LoginForm></LoginForm>
+                </div>
+                <div className={styles.containerLinks}>
+                    <Link href='/roles' className={styles.link}>¿No tienes una cuenta?, registrate aquí</Link>
+                    <Link href='/' className={styles.link}>Volver al inicio</Link>
                 </div>
             </div>
         </div>
