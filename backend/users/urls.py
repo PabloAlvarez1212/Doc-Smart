4c2e2c8870aa46afe856f4789eab45eea25d412c
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-13 12:53:56 -0500

    Agrega el controlador y sus rutas de cambiar y solicitar cambio de contraseña

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, SolicitarCambioView, CambiarContraseñaView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
+    path('solicitar-cambio/', SolicitarCambioView.as_view()),
+    path('cambiar-contraseña/', CambiarContraseñaView.as_view()),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from users.services import loginService
+from users.services import loginService, solicitarCambioService, cambiarContraseñaService
 
 class LoginView(APIView):
     def post(self, request):
@@ -46,6 +46,47 @@ class LoginView(APIView):
                 max_age=3600
             )
             return response
+
+        except Exception as e:
+            return Response(
+                {'error': 'Error interno del servidor'},
+                status=500
+            )
+            
+class SolicitarCambioView(APIView):
+    def post(self, request):
+        #obtenemos el correo del front
+        correo = request.data.get('correo')
+
+        if not correo:
+            return Response(
+                {'error': 'El correo es requerido'},
+                status=400
+            )
+
+        try:
+            mensaje, status_code = solicitarCambioService(correo)
+            return Response({'mensaje': mensaje}, status=status_code)
+
+        except Exception as e:
+            return Response(
+                {'error': 'Error interno del servidor'},
+                status=500
+        )
+
+
+class CambiarContraseñaView(APIView):
+    def post(self, request):
+        token = request.data.get('token')
+        nueva_contraseña = request.data.get('nueva_contraseña')
+        if not token or not nueva_contraseña:
+            return Response(
+                {'error': 'Token y nueva contraseña son requeridos'},
+                status=400
+            )
+        try:
+            mensaje, status_code = cambiarContraseñaService(token, nueva_contraseña)
+            return Response({'mensaje': mensaje}, status=status_code)
 
         except Exception as e:
             return Response(
