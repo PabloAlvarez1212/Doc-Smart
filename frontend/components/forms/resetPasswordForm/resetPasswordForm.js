4a544d1c02bd271c42faf3adf7d891bd3b895fba
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-18 17:12:44 -0500

    feat: Agrega estructura inicial del reset password

---
+++ frontend/components/forms/resetPasswordForm/resetPasswordForm.js
@@ -0,0 +1,7 @@
+import styles from "./resetPasswordForm.module.css"
+import Input from "../../ui/Input/Input"
+import Button from "../../ui/Button/Button"
+import useResetPasswordForm from "./useResetPasswordForm"
+export default function ResetPasswordForm(){
+
+}
---
+++ frontend/src/app/(auth)/reset-password/page.js
@@ -0,0 +1,4 @@
+import styles from "./resetPassword.module.css"
+export default function ResetPassword(){
+    
+}
