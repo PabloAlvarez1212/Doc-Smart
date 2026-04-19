180df8cbd7fcdd7c094c122c112076d4f3c46d00
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-19 10:35:27 -0500

    feat: implementa el diseño de la pagina reset password

--- frontend/components/forms/resetPasswordForm/resetPasswordForm.js
+++ frontend/components/forms/resetPasswordForm/resetPasswordForm.js
@@ -2,6 +2,41 @@ import styles from "./resetPasswordForm.module.css"
 import Input from "../../ui/Input/Input"
 import Button from "../../ui/Button/Button"
 import useResetPasswordForm from "./useResetPasswordForm"
-export default function ResetPasswordForm(){
-
+import { AlertCircleIcon } from "lucide-react"
+export default function ResetPasswordForm() {
+    return (
+        <form className={styles.form}>
+            <div className={styles.containerMain}>
+                <Input className={styles.input} type="password" placeholder="Nueva contraseña" />
+                <ul className={styles.list}>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>La contraseña debe tener mínimo 8 caracteres</li>
+                    </div>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>No se permiten los caracteres (&lt;, &gt;, &quot;, &apos;, &amp;) en la contraseña</li>
+                    </div>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>La contraseña debe contener al menos un carácter especial</li>
+                    </div>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>La contraseña debe tener mínimo una mayúscula</li>
+                    </div>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>La contraseña debe tener mínimo una minúscula</li>
+                    </div>
+                    <div className={styles.listItem}>
+                        <AlertCircleIcon size={30} color="orange" />
+                        <li>La contraseña debe tener mínimo un número</li>
+                    </div>
+                </ul>
+                <Input type="password" className={styles.input} placeholder="Confirmar nueva contraseña" />
+                <Button type="submit">Cambiar contraseña</Button>
+            </div>
+        </form>
+    )
 }
--- frontend/src/app/(auth)/reset-password/page.js
+++ frontend/src/app/(auth)/reset-password/page.js
@@ -1,4 +1,21 @@
 import styles from "./resetPassword.module.css"
+import ResetPasswordForm from "../../../../components/forms/resetPasswordForm/resetPasswordForm"
+import Link from "next/link"
+
 export default function ResetPassword(){
+    return (
+        <div className={styles.containerMain}>
+            <div className={styles.containerCard}>
+                <div className={styles.containerTitle}>
+                    <h1>Cambia tu contraseña</h1>
+                    <p>Crea una nueva contraseña para acceder a tu cuenta.</p>
+                </div>
+                <div className={styles.form}>
+                    <ResetPasswordForm></ResetPasswordForm>
+                </div>
+                <Link href='/' className={styles.link}>Volver al inicio</Link>
+            </div>
+        </div>
+    )
     
 }
