82dcac87d7c75e72c786d2f2c72b8566ca631bb4
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-23 09:44:31 -0500

    refactor: cambia los services a la carpeta auth

---
+++ frontend/src/app/(auth)/services/authService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const API_URL = 'http://localhost:8000/api' //Url de la api del back
+//formData = body
+export const loginService = async (formData) => {
+    const response = await axios.post(`${API_URL}/login/`, formData, { //Envia Un solicitud post al back con el enpoint correspodiente al login
+        withCredentials: true  // para enviar y recibir cookies
+    })
+    return response.data //retorna respuesta del back
+}
+
+export const forgotPasswordService = async (formData) => {
+    const response = await axios.post(`${API_URL}/solicitar-cambio/`, formData)
+    return response.data
+}
+
+export const resetPasswordService = async (formData) =>{
+    const response = await axios.post(`${API_URL}/cambiar-contraseña/`,formData,{
+        withCredentials: true
+    });
+    return response.data;
+}
