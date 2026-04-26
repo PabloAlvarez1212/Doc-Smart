e7e71be257b0581b7d3349a1e63844c5f67d2896
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-26 12:44:59 -0500

    refactor: renombra importaciones

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
-import Features from './_components/Features/Features';
-import Header from './_components/Header/Header';
-import Hero from './_components/Hero/Hero';
-import Footer from './_components/Footer/Footer';
+import Features from './_componentsHome/Features/Features';
+import Header from './_componentsHome/Header/Header';
+import Hero from './_componentsHome/Hero/Hero';
+import Footer from './_componentsHome/Footer/Footer';
 export default function Home() {
   return (
     <>
