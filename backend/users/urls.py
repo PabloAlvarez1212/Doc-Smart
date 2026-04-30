989b89d6e73504def7956f86f1dc2a3257dc8539
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-30 17:54:04 -0500

    feat: implementa el crud de usuarios

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import LoginView, SolicitarCambioView, CambiarContraseñaView
+from .views import (
+    LoginView,
+    SolicitarCambioView,
+    CambiarContraseñaView,
+    UsuarioListView,
+    UsuarioDetailView
+)
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('solicitar-cambio/', SolicitarCambioView.as_view()),
     path('cambiar-contraseña/', CambiarContraseñaView.as_view()),
+    path('usuarios/', UsuarioListView.as_view()),
+    path('usuarios/<int:pk>/', UsuarioDetailView.as_view()),
 ]
