7d4e32249ee0f42dfc57128e2cc97b3f550196b7
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-04-30 17:11:39 -0500

    feat: agrega las tabalas estado,lugar y medio a la db

--- backend/catalogos/models.py
+++ backend/catalogos/models.py
@@ -5,4 +5,21 @@ class Rol(models.Model):
 
     def __str__(self):
         return self.nombre
-# Create your models here.
+    
+class Estado(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Lugar(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Medio(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
