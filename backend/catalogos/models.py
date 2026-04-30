a31a266dc5261e98155673cda5d7ca141e1a6d82
Yesid <kleiderecheverry@gmail.com>
2026-04-30 18:11:11 -0500

    crear las tablas Citas y HistorialClinico

--- backend/catalogos/models.py
+++ backend/catalogos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from citas.models import Cita
+from users.models import Usuario
 
 class Rol(models.Model):
     nombre = models.CharField(max_length=50)
@@ -23,3 +25,15 @@ class Medio(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class HistorialClinico(models.Model):
+    diagnostico_general = models.TextField()
+    observaciones = models.TextField(blank=True, null=True)
+    motivo_consulta = models.TextField()
+    cedula = models.CharField(max_length=20)
+
+    cita = models.ForeignKey(Cita, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Historial {self.id} - {self.cedula}"
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'medicos',
     'catalogos',
+    'citas',
     'corsheaders',
 ]
 
