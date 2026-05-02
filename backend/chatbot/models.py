042e5b49f4fccc0c241fcae30588d56febfa31e5
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-05-02 18:41:36 -0500

    refactor: cambia las tablas del modelo chat bot

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 from users.models import Usuario
 
-
-class Chatbot(models.Model):
-
-    usuario = models.ForeignKey(
-        Usuario,
-        on_delete=models.CASCADE,
-        related_name='chatbots'
-    )
-
-    pregunta = models.TextField()
-
-    respuesta = models.TextField()
-
+class Chat(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
-
-    estado = models.BooleanField(default=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Chatbot {self.id} - {self.usuario.nombre}"
+        return f"Chat {self.id} - Usuario {self.id_usuario}"
+
+class Mensaje(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    contenido = models.TextField()
+    es_bot = models.BooleanField(default=False)
+    id_chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Mensaje {self.id} - {'Bot' if self.es_bot else 'Usuario'}"
