25aa992e04d7a170b937d3e78da43efbd9a6111e
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-05-02 18:17:25 -0500

    feat: agrega la tabla recordatorio de citas y cambia campos de la tabla de citas

--- backend/citas/models.py
+++ backend/citas/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+from users.models import Usuario
+from medicos.models import Medico
+from catalogos.models import Estado, Lugar, Medio
 
 class Cita(models.Model):
     fecha_programada = models.DateTimeField()
     fecha_final = models.DateTimeField(null=True, blank=True)
+    id_estado = models.ForeignKey(Estado, on_delete=models.PROTECT, null=True, blank=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True, blank=True)
+    id_medico = models.ForeignKey(Medico, on_delete=models.CASCADE, null=True, blank=True)
+    id_lugar = models.ForeignKey(Lugar, on_delete=models.PROTECT, null=True, blank=True)
 
-    estado = models.ForeignKey('catalogos.Estado', on_delete=models.PROTECT)
-    usuario = models.ForeignKey('users.Usuario', on_delete=models.CASCADE)
-    medico = models.ForeignKey('medicos.Medico', on_delete=models.CASCADE)
-    lugar = models.ForeignKey('catalogos.Lugar', on_delete=models.SET_NULL, null=True)
-
-    creada_en = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return f"Cita {self.id} - {self.usuario} - {self.fecha_programada}"
+        return f"Cita {self.id} - {self.fecha_programada}"
+
+class RecordatorioCita(models.Model):
+    fecha_programada = models.DateTimeField()
+    fecha_envio_recordatorio = models.DateTimeField()
+    id_cita = models.ForeignKey(Cita, on_delete=models.CASCADE)
+    id_estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    id_medios = models.ForeignKey(Medio, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Recordatorio {self.id} - Cita {self.id_cita.id}"
