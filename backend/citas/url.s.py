9f576f94cc695f0d44fa7b476a186eb09a914fe0
Yesid <kleiderecheverry@gmail.com>
2026-05-06 19:51:38 -0500

    refactor: delete archive url.s from citas

--- backend/citas/url.s.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CitaListView, CitaDetailView, RecordatorioListView, RecordatorioDetailView
-
-urlpatterns = [
-    path('citas/', CitaListView.as_view(), name='cita-list'),
-    path('citas/<int:pk>/', CitaDetailView.as_view(), name='cita-detail'),
-    path('recordatorios/', RecordatorioListView.as_view(), name='recordatorio-list'),
-    path('recordatorios/<int:pk>/', RecordatorioDetailView.as_view(), name='recordatorio-detail'),
-]
