6ca86ee769a6cb178acdbb41f9f68e8340b862db
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-06-10 17:28:47 -0500

    feat: implementa el header de la pagina del admin

--- frontend/components/admin/Header/Header.js
+++ frontend/components/admin/Header/Header.js
@@ -1,33 +1,41 @@
+"use client"
 import Styles from "./Header.module.css";
 import Image from "next/image";
 import { Settings, UserCircle2Icon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function Header() {
+    const pathName = usePathname(); 
+    const activarLink = function (route){
+        return pathName === route ? Styles.activar : Styles.link;
+    }
     return (
         <div className={Styles.containerHeader}>
             <div className={Styles.container}>
                 <div className={Styles.logo}>
                     <Image src='/images/logo.png' width='130' height='100' alt="logo"/>
-                    <h2><span>Doc</span> Smart</h2>
-                    <UserCircle2Icon/>
-                    <Settings/>
+                    <h2><span>Doc</span>Smart</h2>
                 </div>
-                <div className={Styles.icons}></div>
+                <div className={Styles.icons}>
+                    <UserCircle2Icon size={40} className={Styles.icon}/>
+                    <Settings size={40} color="#262626" className={Styles.icon}/>
+                </div>
             </div>
             <div className={Styles.containerNav}>
                 <nav>
                     <ul>
-                        <li><Link href={'/admin/patients'}>Pacientes</Link></li>
-                        <li><Link href={'/admin/doctors'}>Medicos</Link></li>
-                        <li><Link href={'/admin/specialties'}>Especialidades</Link></li>
-                        <li><Link href={'/admin/cities'}>Ciudades</Link></li>
-                        <li><Link href={'/admin/departments'}>Departamentos</Link></li>
-                        <li><Link href={'/admin/states'}>Estados</Link></li>
-                        <li><Link href={'/admin/channel'}>Medios</Link></li>
-                        <li><Link href={'/admin/roles'}>Roles</Link></li>
+                        <li><Link href={'/admin/patients'} className={activarLink('/admin/patients')}>Pacientes</Link></li>
+                        <li><Link href={'/admin/doctors'} className={activarLink('/admin/doctors')}>Medicos</Link></li>
+                        <li><Link href={'/admin/specialties'} className={activarLink('/admin/specialties')}>Especialidades</Link></li>
+                        <li><Link href={'/admin/cities'} className={activarLink('/admin/cities')}>Ciudades</Link></li>
+                        <li><Link href={'/admin/departments'} className={activarLink('/admin/departments')}>Departamentos</Link></li>
+                        <li><Link href={'/admin/states'} className={activarLink('/admin/states')}>Estados</Link></li>
+                        <li><Link href={'/admin/channel'} className={activarLink('/admin/channel')}>Medios</Link></li>
+                        <li><Link href={'/admin/roles'} className={activarLink('/admin/roles')}>Roles</Link></li>
                     </ul>
                 </nav>
             </div>
+            <div className={Styles.line}></div>
         </div>
     )
 }
