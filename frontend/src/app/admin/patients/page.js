37b721aea54342e2cc5d7052e3b28bbe5a78d9dd
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-07-12 13:57:08 -0500

    feat: implementa los enpoints requeridos para el funcionamiento del la pagina del paciente(admin) y utiliza el hero y el data table en patients/page.js

--- frontend/src/app/admin/patients/page.js
+++ frontend/src/app/admin/patients/page.js
@@ -1,5 +1,53 @@
-export default function Patients(){
-    return(
-        <h2>Page home</h2>
+"use client"
+
+import Hero from "../../../../components/admin/Paciente/Hero/Hero"
+import DataTable from "../../../../components/ui/DataTable/DataTable"
+import styles from "./patients.module.css"
+import { useCrud } from "../../../../components/hooks/useCrud";
+import { getPacientesService,deletePacienteService } from "@/app/services/adminServices";
+export default function Patients() {
+    const crud = useCrud({
+        getService: getPacientesService,
+        eliminarService: deletePacienteService
+    })
+    const columnas = [
+        {
+            key: "id",
+            label: "Id",
+        },
+        {
+            key: "nombre",
+            label: "Nombre",
+        },
+        {
+            key: "apellido",
+            label: "Apellido"
+        },
+        {
+            key: "telefono",
+            label: "Telefono"
+        },
+        {
+            key: "correo",
+            label: "Correo"
+        },
+    ];
+    return (
+        <>
+            <Hero />
+            <div className={styles.containerTable}>
+                <DataTable
+                    titulo="Pacientes"
+                    columnas={columnas}
+                    crear={false}
+                    cargando={crud.cargando}
+                    datos={crud.datos}
+                    mostrarEditar={false}
+                    centrarAcciones={true}
+                    onEliminar={crud.eliminar}
+                />
+            </div>
+
+        </>
     )
 }
