7e39828f584c4a6785d6df455b33894f73b833b4
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-07-13 19:58:08 -0500

    feat: implementa en la pagina del doctor(admin) el hero y su respectiva tabla de doctores

--- frontend/src/app/admin/doctors/page.js
+++ frontend/src/app/admin/doctors/page.js
@@ -1,9 +1,68 @@
+"use client";
 import Hero from "../../../../components/admin/Doctor/Hero";
+import { useCrud } from "../../../../components/hooks/useCrud";
+import DataTable from "../../../../components/ui/DataTable/DataTable";
+import { getDoctoresService, deleteDoctorService } from "@/app/services/adminServices";
+import styles from "./doctors.module.css";
 
 export default function Doctors() {
+  const crud = useCrud({
+    getService: getDoctoresService,
+    eliminarService: deleteDoctorService
+  })
+  const columnas = [
+    {
+      key: "id",
+      label: "Id",
+    },
+    {
+      key: "nombre",
+      label: "Nombre",
+    },
+    {
+      key: "apellido",
+      label: "Apellido"
+    },
+    {
+      key: "especialidad",
+      label: "Especialidad"
+    },
+    {
+      key: "ciudad",
+      label: "Ciudad"
+    },
+    {
+      key: "departamento",
+      label: "Departamento"
+    },
+    {
+      key: "direccion",
+      label: "Direccion"
+    },
+    {
+      key: "telefono",
+      label: "Telefono"
+    },
+    {
+      key: "correo",
+      label: "Correo"
+    },
+  ];
+
   return (
     <>
       <Hero />
+      <div className={styles.containerTable}>
+        <DataTable
+          centrarAcciones={true}
+          mostrarEditar={false}
+          titulo="Doctores"
+          onEliminar={crud.eliminar}
+          datos={crud.datos}
+          cargando={crud.cargando}
+          columnas={columnas}
+        />
+      </div>
     </>
   );
 }
