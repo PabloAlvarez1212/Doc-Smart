5144a11ff0776761378ba71832162cd6467ec0c9
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-07-20 18:29:03 -0500

    refactor: implementa el componente Hero en la pagina Home del paciente

--- frontend/src/app/patient/home/page.js
+++ frontend/src/app/patient/home/page.js
@@ -1,21 +1,11 @@
 import styles from './home.module.css'
 import Button from '../../../../components/ui/Button/Button'
 import Image from 'next/image'
+import Hero from '../../../../components/patient/Home/Hero/Hero'
 export default function Home() {
     return (
-        <div className={styles.containerMain}>
-            <div className={styles.saludo}>
-                <div className={styles.containerText}>
-                    <p>¡Bienvenido de vuelta!,</p>
-                    <h2>Pablo</h2>
-                    <p>Tienes 2 citas próximas y 3 notificaciones sin leer.</p>
-                    <Button size='sm' variant='white'>Ver más&nbsp;&nbsp;&nbsp;&gt;</Button>
-                </div>
-
-                <div className={styles.img}>
-                    <Image src='/images/messias.jpg' alt='foto de perfi' width={150} height={100}></Image>
-                </div>
-            </div>
-        </div>
+        <>
+            <Hero/>
+        </>
     )
 }
