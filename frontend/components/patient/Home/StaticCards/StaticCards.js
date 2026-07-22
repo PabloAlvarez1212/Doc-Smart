b97a3b7890d9ff72c97b0a128122caa790786607
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-07-21 19:37:07 -0500

    style: elimina los bordes de las cards en la pagina home del paciente

--- frontend/components/patient/Home/StaticCards/StaticCards.js
+++ frontend/components/patient/Home/StaticCards/StaticCards.js
@@ -5,16 +5,16 @@ export default function StaticCards() {
         <div className={styles.containerMain}>
             <div className={styles.containerCards}>
                 <div className={styles.card}>
-                    <Cards image="/icons/cita_medica.png" title="2" align="center" description="Próximas citas" />
+                    <Cards className={styles.itemCard} image="/icons/cita_medica.png" title="2" align="center" description="Próximas citas" />
                 </div>
                 <div className={styles.card}>
-                    <Cards image="/icons/pendiente.png" title="2" align="center" description="Consultas pendientes por confirmar" />
+                    <Cards className={styles.itemCard} image="/icons/pendiente.png" title="2" align="center" description="Consultas pendientes por confirmar" />
                 </div>
                 <div className={styles.card}>
-                    <Cards image="/icons/realizado.png" title="2" align="center" description="Consultas realizadas este mes" />
+                    <Cards className={styles.itemCard} image="/icons/realizado.png" title="2" align="center" description="Consultas realizadas este mes" />
                 </div>
                 <div className={styles.card}>
-                    <Cards image="/icons/notificacion.png" title="2" align="center" description="Notificaciones" />
+                    <Cards className={styles.itemCard} image="/icons/notificacion.png" title="2" align="center" description="Notificaciones" />
                 </div>
             </div>
         </div>
