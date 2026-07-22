00e5e67e8174eea8f1f6de6b12e4867516f7555c
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-07-21 20:02:25 -0500

    feat: implementa el mockup de la seccion de proximas citas a la pagina home del paciente

--- frontend/src/app/patient/home/page.js
+++ frontend/src/app/patient/home/page.js
@@ -1,10 +1,12 @@
 import Hero from '../../../../components/patient/Home/Hero/Hero'
 import StaticCards from '../../../../components/patient/Home/StaticCards/StaticCards'
+import AppointmentsList from '../../../../components/patient/Home/AppointmentsList/AppointmentsList'
 export default function Home() {
     return (
         <>
             <Hero/>
             <StaticCards/>
+            <AppointmentsList />
         </>
     )
 }
