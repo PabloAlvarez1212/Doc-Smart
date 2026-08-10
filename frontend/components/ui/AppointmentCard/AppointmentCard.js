fb9e7d53fe33dedc7d1724954a3204366a2eb563
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-08-09 22:35:03 -0500

    feat: crea un componente reutilizable para las cards del las citas

---
+++ frontend/components/ui/AppointmentCard/AppointmentCard.js
@@ -0,0 +1,84 @@
+import style from "./AppointmentCard.module.css"
+import { estadoDiseño } from "@/app/utils/estadoDise/estadoDiseUtils";
+import formatearFecha from "@/app/utils/fechaFormaterUtils";
+import Image from "next/image";
+import Button from "../Button/Button";
+import { MapPin, Calendar, Clock, XCircle } from "lucide-react";
+export default function AppointmentCard({ cita, rol, cancelarCita }) {
+    const { fecha, hora } = formatearFecha(cita.fecha_programada)
+    const renderAcciones = () => {
+        switch (cita.estado) {
+            case "reprogramada":
+            case "pendiente":
+                return (
+                    <div className={style.btns}>
+                        <Button variant="warning">Reprogramar</Button>
+                        <Button onClick={() => cancelarCita(cita.id)} variant="danger">Cancelar</Button>
+                        {rol === "medico" && (
+                            <Button className={style.btnConfirmar}>Confirmar</Button>
+                        )}
+                    </div>
+                )
+            case "confirmada":
+                return (
+                    <div className={style.btns}>
+                        <Button variant="warning">Reprogramar</Button>
+                        <Button variant="danger">Cancelar</Button>
+                    </div>
+                )
+            case "cancelada":
+                return (
+                    <p className={style.textCanelada}>La cita ha sido cancelada</p>
+                )
+            case "completada":
+                return (
+                    <p className={style.textCompletada}>La cita ha sido completada</p>
+                )
+            default: return null;
+        }
+    }
+    return (
+
+        <div className={style.card}>
+            <div className={style.containerHeader}>
+                <div className={style.info}>
+                    <Image src="/images/doctor3.jpg" alt="Foto" height={70} width={70} />
+                    <div className={style.textHeader}>
+                        <h3>{rol === "paciente" ? cita.medico : cita.paciente}</h3>
+                        <p>{cita.especialidad}</p>
+                    </div>
+                </div>
+                <p className={estadoDiseño(cita.estado)}>{cita.estado}</p>
+                {(cita.estado === "confirmada" || cita.estado === "cancelada") && (
+                    <XCircle
+                        color="red"
+                        className={style.iconX}
+                    />
+                )}
+            </div>
+            <div className={style.main}>
+                <div className={style.infoCita}>
+                    <div className={style.textInfo}>
+                        <Calendar />
+                        <p>{fecha}</p>
+                    </div>
+                    <div className={style.textInfo}>
+                        <Clock />
+                        <p>{hora}</p>
+                    </div>
+                    <div className={style.direccion}>
+                        <MapPin />
+                        <div className={style.textInfoDireccion}>
+                            <p>{`${cita.ciudad} - ${cita.departamento}`}</p>
+                            <p>{cita.direccion}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.footer}>
+                    {renderAcciones()}
+                </div>
+            </div>
+        </div>
+
+    );
+}
