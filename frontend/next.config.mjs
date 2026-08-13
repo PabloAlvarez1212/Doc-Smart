f87491865ccd600a12f550ccf3eb7dec275e57f8
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-08-12 21:47:20 -0500

    config: permitir imágenes del backend local en Next.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
+      },
+    ],
+    //solo en desarrollo
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
