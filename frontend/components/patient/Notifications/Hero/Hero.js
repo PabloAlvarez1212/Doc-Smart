8eb9af32982555cd57c4d5e377bb707563ffe666
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-08-19 22:28:54 -0500

    feat: integra boton para eliminar todas las notificaciones y cambia diseño del btn marcar todas como leìdas

--- frontend/components/patient/Notifications/Hero/Hero.js
+++ frontend/components/patient/Notifications/Hero/Hero.js
@@ -1,15 +1,17 @@
 "use client"
 import Styles from "./Hero.module.css"
 import Button from "../../../ui/Button/Button"
-export default function Hero({noLeidas, marcarTodasLeidas}){
-    return(
+import { CheckCheck, Trash2, } from "lucide-react"
+export default function Hero({ noLeidas, marcarTodasLeidas }) {
+    return (
         <div className={Styles.containerHero}>
             <div className={Styles.Header}>
                 <h2>Notificaciones</h2>
                 <p>Tienes {noLeidas ?? 0} notificaciones sin leer</p>
             </div>
-            <div className={Styles.btn}>
-                <Button onClick={() => marcarTodasLeidas()}>Marcar todas como leidas</Button>
+            <div className={Styles.accionesGlobales}>
+                <Button className={Styles.btnLeerTodas} onClick={marcarTodasLeidas} disabled={noLeidas === 0}><CheckCheck size={18} />Marcar todas como leídas</Button>
+                <Button className={Styles.btnEliminarTodas}><Trash2 size={18} />Eliminar todas</Button>
             </div>
         </div>
     )
