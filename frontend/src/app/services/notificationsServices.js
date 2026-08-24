8d1ce232ad4f900100422a682bec2127d951e8af
Miguel Racero <miguelangelracero05@gmail.com>
2026-08-23 23:26:02 -0500

    fix(notificaciones): corregir integracion y filtros para medico y paciente

--- frontend/src/app/services/notificationsServices.js
+++ frontend/src/app/services/notificationsServices.js
@@ -8,9 +8,10 @@ export const obtenerNotificacionesService = async () => {
     return response.data;
 };
 
-export const marcarNotificacionLeidaService = async (
+
+export const marcarNotificacionLeidaService = async function (
     id_notificacion
-) => {
+) {
     const response = await api.patch(
         `/notificaciones/${id_notificacion}/leida/`
     );
@@ -18,7 +19,8 @@ export const marcarNotificacionLeidaService = async (
     return response.data;
 };
 
-export const marcarTodasNotificacionesLeidasService = async () => {
+
+export const marcarTodasNotificacionesLeidasService = async function () {
     const response = await api.patch(
         "/notificaciones/leida/todas/"
     );
@@ -26,15 +28,17 @@ export const marcarTodasNotificacionesLeidasService = async () => {
     return response.data;
 };
 
-export const eliminarNotificacionService = async (
+
+export const eliminarNotificacionService = async function (
     id_notificacion
-) => {
+) {
     const response = await api.delete(
         `/notificaciones/${id_notificacion}/eliminar/`
     );
 
     return response.data;
 };
+
 
 export const eliminarTodasNotificacionesService = async () => {
     const response = await api.delete(
