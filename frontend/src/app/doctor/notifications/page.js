ef7346bc4db3e2325b2f6fb6a6e01b247b326b2a
Miguel Racero <miguelangelracero05@gmail.com>
2026-08-23 21:50:46 -0500

    feat(notificaciones-medico): completar gestion de notificaciones

--- frontend/src/app/doctor/notifications/page.js
+++ frontend/src/app/doctor/notifications/page.js
@@ -1,7 +1,51 @@
-export default function Notifications(){
-    return(
+"use client";
+
+import Hero from "../../../../components/patient/Notifications/Hero/Hero";
+import NotificationsList from "../../../../components/patient/Notifications/NotificationsList/NotificationsList";
+import useProfile from "../../../../components/doctor/Profile/useProfile";
+import { useNotificaciones } from "../../../../components/hooks/useNotificaciones";
+
+export default function Notifications() {
+
+    const {
+        perfil,
+        loading
+    } = useProfile();
+
+    const {
+    marcarLeida,
+    marcarTodasLeidas,
+    eliminarNotificacion,
+    eliminarTodasNotificaciones,
+    noLeidas,
+    notificaciones,
+    loading: loadingNotificaciones
+} = useNotificaciones(perfil?.id);
+
+    if (loading || loadingNotificaciones) {
+        return <p>Cargando notificaciones...</p>;
+    }
+
+    return (
         <div>
-            <h1>Mis Notificación - Medico</h1>
+
+            <Hero
+                noLeidas={noLeidas}
+                marcarTodasLeidas={marcarTodasLeidas}
+                notificaciones={notificaciones}
+                eliminarTodas={eliminarTodasNotificaciones}
+            />
+
+            <NotificationsList
+                data={{
+                    ...perfil,
+                    notificaciones
+                }}
+                marcarLeida={marcarLeida}
+                eliminarNotificacion={eliminarNotificacion}
+                eliminarTodasNotificaciones={eliminarTodasNotificaciones}
+            />
+
         </div>
-    )
+    );
 }
--- frontend/src/app/services/notificationsServices.js
+++ frontend/src/app/services/notificationsServices.js
@@ -1,27 +1,45 @@
-import api from './api'
-const API_URL = 'http://localhost:8000/api/'
+import api from "./api";
 
 export const obtenerNotificacionesService = async () => {
-    const response = await api.get("/notificaciones/")
-    return response.data
-}
+    const response = await api.get(
+        "/notificaciones/"
+    );
 
-export const marcarNotificacionLeidaService = async function (id_notificacion) {
-    const response = await api.patch(`${API_URL}notificaciones/${id_notificacion}/leida/`);
     return response.data;
 };
 
-export const marcarTodasNotificacionesLeidasService = async function () {
-    const response = await api.patch('notificaciones/leida/todas');
-    return response.data
-}
+export const marcarNotificacionLeidaService = async (
+    id_notificacion
+) => {
+    const response = await api.patch(
+        `/notificaciones/${id_notificacion}/leida/`
+    );
 
-export const eliminarNotificacionService = async function (id_notificacion) {
-    const response = await api.delete(`notificaciones/${id_notificacion}/eliminar/`)
     return response.data;
-}
+};
+
+export const marcarTodasNotificacionesLeidasService = async () => {
+    const response = await api.patch(
+        "/notificaciones/leida/todas/"
+    );
+
+    return response.data;
+};
+
+export const eliminarNotificacionService = async (
+    id_notificacion
+) => {
+    const response = await api.delete(
+        `/notificaciones/${id_notificacion}/eliminar/`
+    );
+
+    return response.data;
+};
 
 export const eliminarTodasNotificacionesService = async () => {
-    const response = await api.delete(`notificaciones/eliminar/todas/`)
-    return response.data
-}
+    const response = await api.delete(
+        "/notificaciones/eliminar/todas/"
+    );
+
+    return response.data;
+};
