21971060972affbe742144ad2df69fe15437b3f4
PabloAlvarez1212 <juanpalvarezm5@gmail.com>
2026-08-26 21:40:30 -0500

    feat: integra estructura inicial de la pagina historial clinico en el paciente

--- frontend/components/patient/layout/Header/Header.js
+++ frontend/components/patient/layout/Header/Header.js
@@ -35,6 +35,7 @@ export default function Header() {
                     <ul>
                         <li><Link href='/patient/home' className={activateLink('/patient/home')} >Inicio</Link></li>
                         <li><Link href='/patient/my-appointments' className={activateLink('/patient/my-appointments')}>Mis citas</Link></li>
+                        <li><Link href='/patient/my-medical-history' className={activateLink('/patient/my-medical-history')}>Historial clínico</Link></li>
                         <li><Link href='/patient/my-profile' className={activateLink('/patient/my-profile')}>Perfil</Link></li>
                         <li><Link href='/patient/chatbot' className={activateLink('/patient/chatbot')}>Chat bot</Link></li>
                         <li><Link href='/patient/' className={activateLink('/patient/')}>Encontar doctores</Link></li>
